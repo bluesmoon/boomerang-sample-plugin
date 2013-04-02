d787c268a5495ccc21b292d4730d132bfd939467
Philip Tellis <philip@lognormal.com>
2013-04-02 00:17:35 -0700

    Step-3: Implement a method to do the actual work
    In this step we implement the method that actually does the work for our
    plugin... in this case, it counts nodes.  The code iterates through the
    list of nodes passed in, gets their count from the document, and adds it
    to the beacon.
    
    We use the BOOMR.addVar method to add a variable to the beacon.
    
    We've also added a new configurable parameter called `prefix`.  It's a
    good idea for all plugins to prefix the variables they add to the beacon
    with a namespace.  This helps avoid conflicts across plugins.
    
    Unfortunately early plugins did not follow this convention, so we've got
    a few variables that are badly named and need to stay that way to be
    backwards compatible.

--- example.js
+++ example.js
@@ -7,13 +7,25 @@ BOOMR.plugins = BOOMR.plugins || {};
 
 var impl = {
 	node_names: [],
+	prefix: "nodecount.",
+
+	// implement a method to do the actual work
+	run: function()
+	{
+		// for this example, we'll ignore browsers that do not support
+		// Array.forEach
+
+		this.node_names.forEach(function(node_name) {
+			BOOMR.addVar(this.prefix + node_name, d.getElementsByTagName(node_name).length);
+		});
+	},
 
 	complete: false
 };
 
 BOOMR.plugins.NodeCounter = {
 	init: function(config) {
-		var i, properties = ["node_names"];
+		var i, properties = ["node_names", "prefix"];
 
 		BOOMR.utils.pluginConfig(impl, config, "NodeCounter", properties);
 
